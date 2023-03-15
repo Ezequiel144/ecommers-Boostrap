fa9039606a16b86a635fef2c37eb279c4caef42b
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-03-15 16:35:15 +0100

    proceso 2

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ const product = [
         img: "./img/prodrem.jpg",
         tipo: "remera",
         cant: 1,
-        id: 1,    
+        id: 1,
+        precio: 100,    
     },
     {
         prod: "Remeras-Admin",
         img: "./img/prodrem.jpg",
         tipo: "remera",
         cant: 1,
-        id: 2,    
+        id: 2,
+        precio: 300,    
     },
     {
         prod: "Remeras-Block",
         img: "./img/prodrem.jpg",
         tipo: "remera",
         cant: 1,
-        id: 3,    
+        id: 3,
+        precio: 150,    
     },
     {
         prod: "Remeras-Fire",
@@ -26,6 +29,7 @@ const product = [
         tipo: "remera",
         cant: 1,    
         id: 4,
+        precio: 220,
     },
 ];
 const sec2 = document.querySelector(".section2");
@@ -34,6 +38,9 @@ const cartshop = document.querySelector(".catshop");
 let addprod = document.querySelectorAll(".agregar");
 const closemenu = document.querySelector(".catshop__x");
 const numbercart = document.querySelector(".content__cart__value");
+const cartshopcontent = document.querySelector(".catshop__content");
+const pricetotal = document.querySelector(".catshop__total__price__number");
+let borrarprod = document.querySelectorAll(".cartshop__prod__delete");
 
 function createprod(productE){
     productE.forEach(p => {
@@ -74,6 +81,8 @@ closemenu.addEventListener("click",() => {
     cartshop.style.display = "none";
 })
 
+
+/* cargar carrito en lista */
 const cartlist = [];
 
 function addtolistcart(e){
@@ -84,7 +93,7 @@ function addtolistcart(e){
     del arreglo que cumpla la condicion y
     retornara ese valor.
     en este caso un objeto en la cte PROADD*/
-    /* console.log(prodadd); */
+    
     /* console.log(cartlist); */
     if(cartlist.some(list => list.id === parseInt(id))){
         /* some(): Prueba si al menos un elemeto pasa la condicion que 
@@ -93,12 +102,15 @@ function addtolistcart(e){
         /* const index = cartlist.findIndex(ind => ind.id === parseInt(id));
         cartlist[index].cant++; */
         prodadd.cant++;
+        /* console.log(prodadd); */
     }else{
         prodadd.cant = 1;
-        cartlist.push(prodadd);//Agrega a la lista//
+        cartlist.push(prodadd);//Agrega a la lista//        
     }
     /* console.log(cartlist); */
     updatenumbcart(cartlist);
+    localStorage.setItem("lista-productos-al-carrito",JSON.stringify(cartlist));
+    addtocart(cartlist);
 }
 
 function updatenumbcart(list){
@@ -107,12 +119,46 @@ function updatenumbcart(list){
     numbercart.innerText = totalcart;
 }
 
+function addtocart(prod){
+    
+    /* cartshop.innerHTML = " "; */
+    cartshopcontent.innerHTML = " ";
+    prod.forEach(p => {
+        const div2 = document.createElement("div");
+        div2.classList = "cartshop__prod";
+        div2.innerHTML = `
+        <img class="cartshop__prod__img" src="${p.img}" alt="${p.id}">
+        <h5 class="cartshop__prod__title">${p.prod}</h5>
+        <img id="${p.id}" class="cartshop__prod__delete" src="./img/borrar.png" alt="borrar">
+        <p class="cartshop__prod__cant">${p.cant}</p>
+        <p class="cartshop__prod__price">${p.precio*p.cant}</p>`;
+        /* cartshop.append(div2); */
+        cartshopcontent.append(div2);
+    })
+    borrarproductos();
+    total();
+}
 
-function addtocart(p){
-    const div2 = document.createElement("div");
-    div2.classList = "cartshop__prod";
-    div2.innerHTML = `<img class="cartshop__prod__img" src="${p.img}" alt="${p.id}">
-    <h5 class="cartshop__prod__title">${p.prod}</h5>
-    <p class="cartshop__prod__cart">0</p`;
-    cartshop.append(div2);
+function total(){
+    const totalcart = cartlist.reduce((acc,prod) => acc + (prod.cant * prod.precio),0);
+    pricetotal.innerText = totalcart; 
+}
+
+function borrarproductos(){
+    borrarprod = document.querySelectorAll(".cartshop__prod__delete");
+    borrarprod.forEach(b => {
+        /* console.log(b); */
+        b.addEventListener("click",eliminarprod);
+    });
+}
+
+
+function eliminarprod(p){
+    let idprod = p.currentTarget.id;
+    /* console.log(idprod); */
+    let indextodelete = cartlist.findIndex(prod => prod.id === idprod);
+    /* console.log(cartlist); */
+    cartlist.splice(indextodelete,1);
+    /* console.log(cartlist); */
+    addtocart(cartlist);
 }
